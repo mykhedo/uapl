1366a950e4f482d12b4808d2513a419a0dffe2f1
mykhedo <mykhaylolevchuk@gmail.com>
2016-10-31 22:33:18 +0200

    added plstests.php

---
+++ pltests.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<html>
+<head>
+	<title>Polski słowa</title>
+</head>
+<body>
+	<p>Stona internetowa jest w trakcie budowy</p>
+	<p>Changed</p>
+	<p><b><?php echo "słowo php";?></b></p>
+</body>
+</html>
